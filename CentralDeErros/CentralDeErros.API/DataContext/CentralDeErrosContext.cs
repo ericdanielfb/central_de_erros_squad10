aeb7c6963bc1ba8c1fc7bb34ff4bcca823e707dd
Marcelo Amorim <marcelo.med.amorim@gmail.com>
2020-07-09 19:54:07 -0300

    Adiciona proposta 3 de projeto

---
+++ CentralDeErros/CentralDeErros.API/DataContext/CentralDeErrosContext.cs
@@ -0,0 +1,20 @@
+﻿using CentralDeErros.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralDeErros.API.DataContext
+{
+    public class CentralDeErrosContext : DbContext
+    {
+        public DbSet<Occurrence> Occurrences { get; set; }
+
+        public CentralDeErrosContext(DbContextOptions<CentralDeErrosContext> options)
+           : base(options)
+        {
+        }
+    }
+}
