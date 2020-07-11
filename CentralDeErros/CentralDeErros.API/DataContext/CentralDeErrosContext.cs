15aeb08ed9f119a410d69a054cc58d847cf68224
Marcelo Martins <dw.cv.mj@gmail.com>
2020-07-11 00:18:23 -0300

    Add Environment, Fks in Occurrence, icollection in Error and Level and Dbset in DbContext

--- CentralDeErros/CentralDeErros.API/DataContext/CentralDeErrosContext.cs
+++ CentralDeErros/CentralDeErros.API/DataContext/CentralDeErrosContext.cs
@@ -3,14 +3,21 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
+using Environment = CentralDeErros.API.Models.Environment;
 
 namespace CentralDeErros.API.DataContext
 {
     public class CentralDeErrosContext : DbContext
     {
         public DbSet<Occurrence> Occurrences { get; set; }
+        public DbSet<Error> Errors { get; set; }
+        public DbSet<Level> Levels { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Microservice> Microservices { get; set; }
+        public DbSet<Environment> Environments { get; set; }
 
         public CentralDeErrosContext(DbContextOptions<CentralDeErrosContext> options)
            : base(options)
